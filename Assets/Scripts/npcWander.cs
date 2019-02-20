b474c68429c917ee53debd36e333320a60ef57f2
Jigg21 <to.mathis21@gmail.com>
2019-02-19 22:56:46 -0600

    Added main scene Added Map prefab Added npc with wander script added morgue sink/table fixed lighting put ceiling lamp models into the scene.
    Signed-off-by: Jigg21 <to.mathis21@gmail.com>

---
+++ Assets/Scripts/npcWander.cs
@@ -0,0 +1,58 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npcWander : MonoBehaviour
+{
+    public GameObject[] locations;
+    public float threshold;
+
+    private Vector3 currentDest;
+    private NavMeshAgent agent;
+    private bool arrived;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        chooseNewDest();
+        currentDest = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!arrived)
+        {
+            if (Vector3.Distance(transform.position,agent.destination) <= threshold)
+            {
+                arrived = true;
+                anim.SetTrigger("Reset");
+                Invoke("chooseNewDest", 5f);
+            }
+        }
+    }
+
+    void chooseNewDest ()
+    {
+        arrived = false;
+        do
+        {
+            int i = Random.Range(0, locations.Length);
+            agent.SetDestination(locations[i].transform.position);
+        } while (agent.destination == currentDest);
+        currentDest = agent.destination;
+        int speed = Random.Range(0, 2);
+        if(speed == 0)
+        {
+            anim.SetTrigger("Walk");
+            agent.speed = 3.5f;
+        }else
+        {
+            anim.SetTrigger("Run");
+            agent.speed = 5f;
+        }
+    }
+}
