c1717aa6df1d44d4c2294cd8b2ec101f0737a887
Mitchell Marshe <mitchellmarshe@gmail.com>
2019-02-20 16:25:42 -0600

    PC Build

---
+++ Assets/Scripts/HauntActions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HauntActions : ItemActionInterface
+{
+    public string[] myActionNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myActionNames = new string[] { "Haunt", "...", "Unpossess", "..." };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void callAction1()
+    {
+
+    }
+
+    public override void callAction2()
+    {
+
+    }
+
+    public override void callAction3()
+    {
+
+    }
+
+    public override void callAction4()
+    {
+
+    }
+
+    public override string[] getActionNames()
+    {
+        return myActionNames;
+    }
+
+    public override void setActionNames(string[] names)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            myActionNames[i] = names[i];
+        }
+    }
+}
