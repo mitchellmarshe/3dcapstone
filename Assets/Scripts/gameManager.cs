5fb7e8067caa3dd2dda710a3d236db7a0b68d9dc
Jigg21 <to.mathis21@gmail.com>
2019-04-07 16:02:27 -0500

    Added Animations for hypno lizard Started GameManager for keeping track of game state
    Signed-off-by: Jigg21 <to.mathis21@gmail.com>

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    private int npcCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        npcCount = GameObject.FindGameObjectsWithTag("NPC").Length;
+    }
+    
+    //player kills an npc
+    public void killNPC ()
+    {
+        npcCount -= 1;
+        if (npcCount <= 0)
+        {
+            endGame();   
+        }
+    }
+
+    //what happens when the player kills all npcs
+    private void endGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
