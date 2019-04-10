ff63e9a5c039f5fb80caa3f994151d3e3daf4312
Huylar Lee <huylarlee@utexas.edu>
2019-04-10 15:24:16 -0500

    Tutorial system

--- Assets/Scripts/PlayStartAnimation.cs
+++ Assets/Scripts/PlayStartAnimation.cs
@@ -7,12 +7,14 @@ public class PlayStartAnimation : MonoBehaviour
     public GameObject player;
     public Menu menu;
     public GameObject decals;
+    public GameObject tutorial;
     // Start is called before the first frame update
     private void Start()
     {
         player.GetComponentInChildren<Camera>().enabled = false;
         player.GetComponentInChildren<Controller2>().enabled = false;
         decals.SetActive(false);
+        tutorial.SetActive(false);
         menu.fade.Play("reverseDeath");
         Invoke("activePlayer", 14);
     }
@@ -22,6 +24,7 @@ public class PlayStartAnimation : MonoBehaviour
         player.GetComponentInChildren<Camera>().enabled = true;
         player.GetComponentInChildren<Controller2>().enabled = true;
         decals.SetActive(true);
+        tutorial.SetActive(true);
         gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/TutorialSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] tips;
+    private int i = 0;
+
+    public void nextTip()
+    {
+        if(i >= tips.Length)
+        {
+            gameObject.SetActive(false);
+        } else
+        {
+            i++;
+            tips[i - 1].SetActive(false);
+            tips[i].SetActive(true);
+        }
+        
+    }
+}
