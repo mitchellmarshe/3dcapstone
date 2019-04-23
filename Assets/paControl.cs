90fe4a3724f24962560ab04e9edafd1929e88bef
Jigg21 <to.mathis21@gmail.com>
2019-04-23 12:01:55 -0500

    Added ambient sounds: 	AC unit 	Vending Machine 	PA System Callout 	PA System Music Added PA system which switches between calls and music
    Signed-off-by: Jigg21 <to.mathis21@gmail.com>

---
+++ Assets/paControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paControl : MonoBehaviour
+{
+    public AudioClip[] calls;
+    public AudioSource musicSource;
+    public AudioSource callSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(callOut());
+    }
+
+    IEnumerator callOut ()
+    {
+        while (true)
+        {
+            float timetowait = Random.Range(30f, 240f);
+            yield return new WaitForSeconds(timetowait);
+            AudioClip randomCall = calls[Random.Range(0, calls.Length)];
+            musicSource.mute = true;
+            callSource.clip = randomCall;
+            callSource.Play();
+            yield return new WaitForSeconds(randomCall.length);
+            musicSource.mute = false;
+        }
+    }
+
+    
+}
