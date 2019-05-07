93a7a6fb6e9de8fa28ddb5678aa42bae4c084199
Mitchell Marshe <mitchellmarshe@gmail.com>
2019-05-07 14:45:33 -0500

    Adding Splash screen

---
+++ Assets/Splash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public GameObject gddp;
+    public GameObject logo;
+
+    void Start()
+    {
+        Gddp();
+        Invoke("Logo", 3);
+        Invoke("Game", 6);
+    }
+
+    public void Gddp()
+    {
+        gddp.SetActive(true);
+        logo.SetActive(false);
+    }
+
+    public void Logo()
+    {
+        gddp.SetActive(false);
+        logo.SetActive(true);
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
